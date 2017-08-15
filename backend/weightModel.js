e1db94350b34330e9ec7b1818e61713c20622217
Adam Hansson <adaha287@student.liu.se>
2017-08-15 14:21:07 +0200

    Add date to model, default value is today

--- backend/weightModel.js
+++ backend/weightModel.js
@@ -2,7 +2,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var weightSchema = new Schema({
-    w: {type: Number, min: 40, max: 90, required:true}
+    w: {type: Number, min: 40, max: 90, required: true},
+    date: {type: Date, required: true, default: Date.now}
 });
 
 
