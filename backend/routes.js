a7a9a76ab2e372427d8cee79e55b20602975ee05
Adam Hansson <adaha287@student.liu.se>
2017-08-15 14:34:37 +0200

    Add semicolons

--- backend/routes.js
+++ backend/routes.js
@@ -5,14 +5,14 @@ module.exports = function(router){
 
     /* Log a weight */
     router.post('/logWeight', function(req, res){
-        var weight = new Weight()
+        var weight = new Weight();
         var weight.w = req.body.weight;
         if(weight.w > 40.0 && weight.w < 90.0){
-            
-            res.json({success: true, message: 'Saved weight'})
+            weight.save()
+            res.json({success: true, message: 'Saved weight'});
         }
         else{
-            res.json({success: false, message: 'Weight not correctly entered, should be between 40 and 90 kilograms'})
+            res.json({success: false, message: 'Weight not correctly entered, should be between 40 and 90 kilograms'});
         }
     });
     return router;
