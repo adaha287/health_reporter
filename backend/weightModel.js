08317f2e89f056dc20ca3fcd07354bdfded27989
Adam Hansson <adaha287@student.liu.se>
2017-08-15 14:07:31 +0200

    Update min/max value of weight

--- backend/weightModel.js
+++ backend/weightModel.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var weightSchema = new Schema({
-    w: {type: Number, required:true}
+    w: {type: Number, min: 40, max: 90, required:true}
 });
 
 
